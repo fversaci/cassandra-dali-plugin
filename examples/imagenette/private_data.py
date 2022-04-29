f01a439b0d2e9368d491fc8b6dec78f14ea03e87
Francesco Versaci <francesco.versaci@gmail.com>
2022-04-29 14:50:29 +0200

    Added private_data.py

---
+++ examples/imagenette/private_data.py
@@ -0,0 +1,9 @@
+# Copyright 2021-2 CRS4
+#
+# Use of this source code is governed by an MIT-style
+# license that can be found in the LICENSE file or at
+# https://opensource.org/licenses/MIT.
+
+cass_user = "guest"
+cass_pass = "test"
+cassandra_ip = "127.0.0.1"
