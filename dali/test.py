f90bf280aa6761bf61c337ac8e1afe93b5caf2d0
Francesco Versaci <francesco.versaci@gmail.com>
2022-07-26 18:31:16 +0200

    Testing uuids

--- dali/test.py
+++ dali/test.py
@@ -4,13 +4,18 @@
 # license that can be found in the LICENSE file or at
 # https://opensource.org/licenses/MIT.
 
+# cassandra
+from cassandradl import CassandraDataset, CassandraListManager
+from cassandra.auth import PlainTextAuthProvider
+# dali
 import nvidia.dali.fn as fn
 from nvidia.dali.pipeline import pipeline_def
 import nvidia.dali.types as types
 from nvidia.dali.plugin.pytorch import DALIGenericIterator
 import nvidia.dali.plugin_manager as plugin_manager
 plugin_manager.load_library('./cpp/build/libcrs4cassandra.so')
-
+# varia
+import pickle
 from tqdm import trange, tqdm
 import numpy as np
 from time import sleep
@@ -18,10 +23,16 @@ import os
 import torch
 from torchvision import transforms
 
-#help(fn.crs4.cassandra)
+# read list of uuids
+dataset_nm="imagenette"
+suff="_jpg"
+split_fn = f"{dataset_nm}{suff}.split"
+with open(split_fn, "rb") as f:
+  x = pickle.load(f)
+uuids = x['row_keys']
+uuids = list(map(str, uuids)) # convert uuids to strings
 
-uuids = ["ciao", "miao"]
-
+# create dali pipeline
 @pipeline_def(batch_size=3, num_threads=1, device_id=1)
 def get_dali_pipeline():
     images = fn.crs4.cassandra(name="CassReader", uuids=uuids)
