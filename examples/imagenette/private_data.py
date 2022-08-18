3e5370c764cde8432258d93ebdcaec04f07c62c6
Francesco Versaci <francesco.versaci@gmail.com>
2022-08-18 14:15:55 +0200

    New private_data format

--- examples/imagenette/private_data.py
+++ examples/imagenette/private_data.py
@@ -4,6 +4,6 @@
 # license that can be found in the LICENSE file or at
 # https://opensource.org/licenses/MIT.
 
-cass_user = "guest"
-cass_pass = "test"
-cassandra_ips = ["127.0.0.1"]
+username = "guest"
+password = "test"
+cass_ips = ["127.0.0.1"]
