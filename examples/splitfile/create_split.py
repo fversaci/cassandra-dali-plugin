e7b633500d5c8e197e61054454ab882dbc724de1
giovanni.busonera <giovanni.busonera@crs4.it>
2023-03-24 11:06:33 +0100

    Added a first version of the description of all CLI parameters

--- examples/splitfile/create_split.py
+++ examples/splitfile/create_split.py
@@ -19,17 +19,19 @@ def main(*, keyspace:'k'=None, table_suffix:'s'=None, id_col:'i'='patch_id', dat
         metadata_ifn=None, metadata_ofn=None, split_ofn:'o'=None, 
         split_ratio:('r', tolist)=[8,2], balance:('b', parse_balance)='original'):
     """
-    Split generator.
+    Create Split: a splitfile generator starting from data stored on a Cassandra db.
 
     :param keyspace: Specify the Cassandra keyspace
     :param table_suffix: Specify the table_suffix (e.g. test is the suffix for data_test and metadata_test tables)
-    :param id_col: The name of the Cassandra column that contains data IDs 
-    :param data_col:
-    :param
-    :param
-    :param
-    :param
-    :param
+    :param id_col: The name of the db column that contains data IDs 
+    :param data_col: The name of the db column where actual data is stored 
+    :param label_col: db column with label 
+    :param label_type: it is the type of the label (a string: 'int', 'image' o 'none')
+    :param metadata_ifn: The input filename of previous cached metadata 
+    :param metadata_ofn: The filename to cache  metadata read from db
+    :param split_ofn: The name of the output splitfile 
+    :param split_ratio: a comma separated values list that specifies the data proportion among desired splits
+    :param balance: balance configuration among classes for each split (it can be a string ('original', 'random') or a a comma separated values list with one entry for each class
     """
 
     isg = imagenet_split_generator()
