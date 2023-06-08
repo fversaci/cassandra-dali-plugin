ccc7f757e07e4d6d191e6422e393787d0d9bc1ee
Francesco Versaci <francesco.versaci@gmail.com>
2023-06-08 12:20:50 +0200

    Add IF (NOT) EXISTS to CQL

--- docker-scripts/test-ade20k.sh
+++ docker-scripts/test-ade20k.sh
@@ -3,7 +3,7 @@
 pgrep -f cassandra || /cassandra/bin/cassandra 2>&1 | grep "state jump to NORMAL" && \
     cd examples/ade20k/ && \
     (SSL_VALIDATE=false /cassandra/bin/cqlsh --ssl -e "SELECT keyspace_name FROM system_schema.keyspaces WHERE keyspace_name='ade20k';" && \
-         SSL_VALIDATE=false /cassandra/bin/cqlsh --ssl -e "DROP KEYSPACE ade20k;") || true && \
+         SSL_VALIDATE=false /cassandra/bin/cqlsh --ssl -e "DROP KEYSPACE IF EXISTS ade20k;") || true && \
     SSL_VALIDATE=false /cassandra/bin/cqlsh --ssl -f create_tables.cql && \
     pgrep -f spark || (/spark/sbin/start-master.sh  && /spark/sbin/start-worker.sh spark://$HOSTNAME:7077) && \
     /spark/bin/spark-submit --master spark://$HOSTNAME:7077 --conf spark.default.parallelism=10 \
@@ -13,7 +13,7 @@ pgrep -f cassandra || /cassandra/bin/cassandra 2>&1 | grep "state jump to NORMAL
     python3 cache_uuids.py --keyspace=ade20k --table-suffix=orig && \
     python3 loop_read.py --keyspace=ade20k --table-suffix=orig && \
     python3 loop_read.py --keyspace=ade20k --table-suffix=orig --use-gpu && \
-    SSL_VALIDATE=false /cassandra/bin/cqlsh --ssl -e "DROP KEYSPACE ade20k;" && \
+    SSL_VALIDATE=false /cassandra/bin/cqlsh --ssl -e "DROP KEYSPACE IF EXISTS ade20k;" && \
     SSL_VALIDATE=false /cassandra/bin/cqlsh --ssl -f create_tables.cql && \
     python3 extract_serial.py /data/ade20k/images/training/ /data/ade20k/annotations/training/ --table-suffix=orig && \
     rm -f ids_cache/* && \
--- docker-scripts/test-imagenette.sh
+++ docker-scripts/test-imagenette.sh
@@ -3,7 +3,7 @@
 pgrep -f cassandra || /cassandra/bin/cassandra 2>&1 | grep "state jump to NORMAL" && \
     cd examples/imagenette/ && \
     (SSL_VALIDATE=false /cassandra/bin/cqlsh --ssl -e "SELECT keyspace_name FROM system_schema.keyspaces WHERE keyspace_name='imagenette';" && \
-         SSL_VALIDATE=false /cassandra/bin/cqlsh --ssl -e "DROP KEYSPACE imagenette;") || true && \
+         SSL_VALIDATE=false /cassandra/bin/cqlsh --ssl -e "DROP KEYSPACE IF EXISTS imagenette;") || true && \
     SSL_VALIDATE=false /cassandra/bin/cqlsh --ssl -f create_tables.cql && \
     pgrep -f spark || (/spark/sbin/start-master.sh  && /spark/sbin/start-worker.sh spark://$HOSTNAME:7077) && \
     /spark/bin/spark-submit --master spark://$HOSTNAME:7077 --conf spark.default.parallelism=10 \
@@ -18,7 +18,7 @@ pgrep -f cassandra || /cassandra/bin/cassandra 2>&1 | grep "state jump to NORMAL
     python3 cache_uuids.py --keyspace=imagenette --table-suffix=val_256_jpg && \
     python3 loop_read.py --keyspace=imagenette --table-suffix=val_256_jpg && \
     python3 loop_read.py --keyspace=imagenette --table-suffix=train_256_jpg --use-gpu && \
-    SSL_VALIDATE=false /cassandra/bin/cqlsh --ssl -e "DROP KEYSPACE imagenette;" && \
+    SSL_VALIDATE=false /cassandra/bin/cqlsh --ssl -e "DROP KEYSPACE IF EXISTS imagenette;" && \
     SSL_VALIDATE=false /cassandra/bin/cqlsh --ssl -f create_tables.cql && \
     python3 extract_serial.py /tmp/imagenette2-320 --split-subdir=train --table-suffix=train_256_jpg && \
     python3 extract_serial.py /tmp/imagenette2-320 --split-subdir=val --table-suffix=val_256_jpg && \
