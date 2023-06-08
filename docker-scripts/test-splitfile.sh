0ce7fa48c508a011cac76cbcf30571071c8fa9a4
Francesco Versaci <francesco.versaci@gmail.com>
2023-06-08 12:22:11 +0200

    Add test for splitfile

---
+++ docker-scripts/test-splitfile.sh
@@ -0,0 +1,20 @@
+#! /bin/bash -x
+
+pgrep -f cassandra || /cassandra/bin/cassandra 2>&1 | grep "state jump to NORMAL" && \
+    cd examples/splitfile/ && \
+    (SSL_VALIDATE=false /cassandra/bin/cqlsh --ssl -e "SELECT keyspace_name FROM system_schema.keyspaces WHERE keyspace_name='imagenette';" && \
+         SSL_VALIDATE=false /cassandra/bin/cqlsh --ssl -e "DROP KEYSPACE IF EXISTS imagenette;") || true && \
+    SSL_VALIDATE=false /cassandra/bin/cqlsh --ssl -f create_tables.cql && \
+    python3 extract_serial.py /tmp/imagenette2-320 --split-subdir=train --img-format=UNCHANGED --table-suffix=orig && \
+    python3 extract_serial.py /tmp/imagenette2-320 --split-subdir=val --img-format=UNCHANGED --table-suffix=orig && \
+    rm -f imagenette_splitfile.pckl metadata.cache && \
+    python3 create_split.py -k imagenette -s orig -r 8,2 --metadata-ofn metadata.cache -o imagenette_splitfile.pckl && \
+    python3 loop_read.py imagenette_splitfile.pckl && \
+    python3 loop_read.py imagenette_splitfile.pckl --use-index=1 && \
+    python3 create_split.py --metadata-ifn metadata.cache -r 8,2 -o imagenette_splitfile.pckl && \
+    python3 loop_read.py imagenette_splitfile.pckl && \
+    python3 loop_read.py imagenette_splitfile.pckl --use-index=1 && \
+    rm -f imagenette_splitfile.pckl metadata.cache && \
+    `### BEGIN COMMENT \
+    ### END COMMENT`
+    echo "--- OK ---"
---
+++ examples/splitfile/loop_read.py
@@ -0,0 +1,164 @@
+# Copyright 2022 CRS4 (http://www.crs4.it/)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# cassandra reader
+from cassandra_reader import get_cassandra_reader, read_uuids
+from crs4.cassandra_utils import get_shard
+
+# dali
+from nvidia.dali.pipeline import pipeline_def
+from nvidia.dali.plugin.base_iterator import LastBatchPolicy
+from nvidia.dali.plugin.pytorch import DALIGenericIterator
+import nvidia.dali.fn as fn
+import nvidia.dali.types as types
+
+# some preconfigured operators
+from fn_shortcuts import (
+    fn_decode,
+    fn_normalize,
+    fn_image_random_crop,
+    fn_resize,
+    fn_crop_normalize,
+)
+
+# varia
+from clize import run
+from tqdm import trange, tqdm
+import math
+import time
+import pickle
+
+# supporting torchrun
+import os
+
+global_rank = int(os.getenv("RANK", default=0))
+local_rank = int(os.getenv("LOCAL_RANK", default=0))
+world_size = int(os.getenv("WORLD_SIZE", default=1))
+
+
+def read_data(
+    split_fn,
+    *,
+    use_index=0,
+    use_gpu=False,
+    epochs=10,
+):
+    """Read images from DB or filesystem, in a tight loop
+
+    :param use_gpu: enable output to GPU (default: False)
+    """
+    if use_gpu:
+        device_id = local_rank
+    else:
+        device_id = types.CPU_ONLY_DEVICE_ID
+
+    data = pickle.load(open(split_fn, "rb"))
+    keyspace = data['keyspace'] 
+    table_suffix = data['table_suffix'] 
+    row_keys = data['row_keys']
+    split = data['split']
+    uuids = row_keys[split[use_index]]        
+
+    bs = 128
+    uuids, real_sz = get_shard(
+        uuids,
+        batch_size=bs,
+        shard_id=global_rank,
+        num_shards=world_size,
+    )
+    chosen_reader = get_cassandra_reader(
+        keyspace,
+        table_suffix,
+        batch_size=bs,
+        prefetch_buffers=4,
+        io_threads=8,
+        name="Reader",
+        comm_threads=1,
+        copy_threads=4,
+        ooo=True,
+        slow_start=4,
+    )
+
+    
+    # create dali pipeline
+    @pipeline_def(
+        batch_size=bs,
+        num_threads=4,
+        device_id=device_id,
+        prefetch_queue_depth=2,
+        #########################
+        # py_start_method="spawn",
+        # enable_memory_stats=True,
+    )
+    def get_dali_pipeline():
+        images, labels = chosen_reader
+
+        ####################################################################
+        # - decode, resize and crop, must use GPU (e.g., --use-gpu)
+        # images = fn_image_random_crop(images)
+        # images = fn_resize(images)
+        # images = fn_crop_normalize(images)
+        ####################################################################
+        if device_id != types.CPU_ONLY_DEVICE_ID:
+            images = images.gpu()
+            labels = labels.gpu()
+        return images, labels
+
+    pl = get_dali_pipeline()
+    pl.build()
+
+    # feed epoch 0 uuid to the pipeline
+    for u in uuids:
+        pl.feed_input("Reader[0]", u)
+
+    ########################################################################
+    # DALI iterator
+    ########################################################################
+    # produce images
+    # consume uuids to get images from DB
+    for _ in range(epochs):
+        # feed next epoch to the pipeline
+        for u in uuids:
+            pl.feed_input("Reader[0]", u)
+        # read data for current epoch
+        for _ in trange(len(uuids)):
+            pl.run()
+        pl.reset()
+
+        
+    ########################################################################
+    # alternatively: use pytorch iterator
+    # (note: decode of images must be enabled)
+    ########################################################################
+    # ddl = DALIGenericIterator(
+    #     [pl],
+    #     ["data", "label"],
+    #     # reader_name="Reader", # works only with file reader
+    #     size=real_sz,
+    #     last_batch_padded=True,
+    #     last_batch_policy=LastBatchPolicy.PARTIAL #FILL, PARTIAL, DROP
+    # )
+    # for _ in range(epochs):
+    #     # feed next epoch to the pipeline
+    #     for u in uuids:
+    #         pl.feed_input("Reader[0]", u)
+    #     # consume data
+    #     for data in tqdm(ddl):
+    #         x, y = data[0]["data"], data[0]["label"]
+    #     ddl.reset()  # rewind data loader
+
+
+# parse arguments
+if __name__ == "__main__":
+    run(read_data)
