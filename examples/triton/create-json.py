bf92c811d33e163820e5a37869b844398a1480f5
Francesco Versaci <francesco.versaci@gmail.com>
2023-10-19 13:22:30 +0200

    save uuids as json for use with perf_analyzer

--- examples/triton/create-json.py
+++ examples/triton/create-json.py
@@ -13,45 +13,12 @@
 # limitations under the License.
 
 
-import sys
-import gevent.ssl
-import tritonclient.http as httpclient
-from tritonclient.utils import InferenceServerException
-import numpy as np
 from cassandra_reader import read_uuids
 from crs4.cassandra_utils import get_shard
-from tqdm import tqdm, trange
-from IPython import embed
-
 import json
-def save_to_json(uuids, in_name="Reader"):
-    l = list()
-    j = dict()
-    j['data'] = l
-    for u in uuids:
-        b = list()
-        for p in u:
-            p = p.tolist()
-            d = dict()
-            d[in_name] = p
-            b.append(d)
-        l.append(b)
-    # save as json
-    with open('uuids.json', 'w') as f:
-        json.dump(j, f)            
 
-def start_inferring():
-    try:
-        triton_client = httpclient.InferenceServerClient(
-            url="127.0.0.1:8000",
-            verbose=False,
-        )
-    except Exception as e:
-        print("channel creation failed: " + str(e))
-        sys.exit(1)
 
-    model_name = "dali"
-
+def save_to_json(in_name="Reader"):
     uuids = read_uuids(
         keyspace="imagenette",
         table_suffix="train_256_jpg",
@@ -63,26 +30,22 @@ def start_inferring():
         shard_id=0,
         num_shards=1,
     )
-    save_to_json(uuids)
-    for _ in range(1):
-        async_requests = []
-        for raw_data in uuids:
-            inputs = []
-            infer = httpclient.InferInput("Reader", raw_data.shape, "UINT64")
-            infer.set_data_from_numpy(raw_data, binary_data=True)
-            inputs.append(infer)
+    l = list()
+    j = dict()
+    j["data"] = l
+    for u in uuids:
+        b = list()
+        for p in u:
+            p = p.tolist()
+            d = dict()
+            d[in_name] = p
+            b.append(d)
+        l.append(b)
+    # save as json
+    with open("uuids.json", "w") as f:
+        json.dump(j, f)
 
-            # Infer with requested Outputs
-            async_requests.append(
-                triton_client.async_infer(
-                    model_name,
-                    inputs=inputs,
-                )
-            )
-        print("Requests sent")
-        for req in tqdm(async_requests):
-            result = req.get_result()
 
 # parse arguments
 if __name__ == "__main__":
-    start_inferring()
+    save_to_json()
--- examples/triton/start-and-fill-db.sh
+++ examples/triton/start-and-fill-db.sh
@@ -9,4 +9,5 @@ python3 extract_serial.py /tmp/imagenette2-320 --split-subdir=val --table-suffix
 rm -f ids_cache/* 
 python3 cache_uuids.py --keyspace=imagenette --table-suffix=train_256_jpg 
 python3 cache_uuids.py --keyspace=imagenette --table-suffix=val_256_jpg 
+python3 create-json.py
 echo "--- DB is now ready ---"
