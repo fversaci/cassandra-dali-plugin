202dff18bf0466a44514508e71f255a52d793b9b
Francesco Versaci <francesco.versaci@gmail.com>
2024-02-24 13:08:50 +0100

    Numpy decoder with Corel-5k example

---
+++ crs4/cpp/numpy.cc
@@ -0,0 +1,45 @@
+// Copyright (c) 2022-2023, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "./dali_numpy.h"
+#include "./numpy.h"
+
+using namespace dali;
+
+namespace crs4 {
+
+  Tensor<CPUBackend> NewReadTensor(InputStream *src, bool pinned) {
+  numpy::HeaderData header;
+  numpy::ParseHeader(header, src);
+  src->SeekRead(header.data_offset, SEEK_SET);
+
+  Tensor<CPUBackend> data;
+  data.set_pinned(pinned);
+  data.Resize(header.shape, header.type());
+  auto ret = src->Read(static_cast<uint8_t*>(data.raw_mutable_data()), header.nbytes());
+  DALI_ENFORCE(ret == header.nbytes(), "Failed to read numpy file");
+
+  if (header.fortran_order) {
+    Tensor<CPUBackend> transposed;
+    transposed.Resize(data.shape(), data.type());
+    SampleView<CPUBackend> input(data.raw_mutable_data(), data.shape(), data.type());
+    SampleView<CPUBackend> output(transposed.raw_mutable_data(), transposed.shape(),
+                                  transposed.type());
+    numpy::FromFortranOrder(output, input);
+    return transposed;
+  }
+  return data;
+}
+
+}  // namespace crs4
---
+++ examples/corel5k/extract_serial.py
@@ -0,0 +1,51 @@
+# Copyright 2024 CRS4 (http://www.crs4.it/)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import extract_common
+from clize import run
+
+
+def save_images(
+    src_dir,
+    npy_dir,
+    label_file,
+    *,
+    img_format="UNCHANGED",
+    keyspace="corel5k",
+    table_suffix="orig",
+):
+    """Save center-cropped images to Cassandra DB or directory
+
+    :param src_dir: Input directory of images
+    :param npy_dir: Directory which will contain the labels as npy files
+    :param label_file: JSON with the labels
+    :param img_format: Format of output images
+    :param keyspace: Name of dataset (for the Cassandra table)
+    :param table_suffix: Suffix for table names
+    """
+    jobs = extract_common.get_jobs(src_dir, npy_dir, label_file)
+    # Read Cassandra parameters
+    from private_data import cass_conf
+
+    extract_common.send_images_to_db(
+        cass_conf=cass_conf,
+        img_format=img_format,
+        keyspace=keyspace,
+        table_suffix=table_suffix,
+    )(jobs)
+
+
+# parse arguments
+if __name__ == "__main__":
+    run(save_images)
