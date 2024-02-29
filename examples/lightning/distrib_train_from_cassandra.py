919f18de4392189924401f762723412b776af3ac
Giovanni Busonera <giovanni.busonera@crs4.it>
2024-02-29 11:22:30 +0000

    Dali pipeline creation code to a separate file. Loggers added. Code refactoring

--- examples/lightning/distrib_train_from_cassandra.py
+++ examples/lightning/distrib_train_from_cassandra.py
@@ -4,16 +4,10 @@
 
 # cassandra reader
 from cassandra_reader import get_cassandra_reader, read_uuids
+from create_dali_pipeline import create_dali_pipeline
 
 import argparse
-import os
-import shutil
-import time
-import math
 
-from IPython import embed
-
-import lightning as L
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -27,18 +21,17 @@ import torch.utils.data
 import torch.utils.data.distributed
 import torchvision.models as models
 
+import lightning as L
 from lightning.pytorch.profilers import PyTorchProfiler
+from lightning.pytorch.loggers import TensorBoardLogger
+from lightning.pytorch.loggers import CSVLogger
 
 try:
     from nvidia.dali.plugin.pytorch import DALIClassificationIterator, LastBatchPolicy
-    from nvidia.dali.pipeline import pipeline_def
-    import nvidia.dali.types as types
-    import nvidia.dali.fn as fn
 except ImportError:
     raise ImportError(
         "Please install DALI from https://www.github.com/NVIDIA/DALI to run this example."
     )
-
 
 def parse():
     model_names = sorted(
@@ -194,109 +187,16 @@ def parse():
     parser.add_argument("--keep-batchnorm-fp32", type=str, default=None)
     parser.add_argument("--loss-scale", type=str, default=None)
     parser.add_argument("--channels-last", type=bool, default=False)
-    parser.add_argument(
-        "-t",
-        "--test",
-        action="store_true",
-        help="Launch test mode with preset arguments",
-    )
+    
     args = parser.parse_args()
     return args
 
 
-# item() is a recent addition, so this helps with backward compatibility.
-def to_python_float(t):
-    if hasattr(t, "item"):
-        return t.item()
-    else:
-        return t[0]
+########################
+### LIGHTNING MODELS ###
+########################
 
-
-@pipeline_def
-def create_dali_pipeline(
-    keyspace,
-    table_suffix,
-    bs,
-    crop,
-    size,
-    source_uuids,
-    dali_cpu=False,
-    is_training=True,
-    prefetch_buffers=82,
-    shard_id=0,
-    num_shards=1,
-    io_threads=4,
-    comm_threads=2,
-    copy_threads=2,
-    wait_threads=2,
-):
-    cass_reader = get_cassandra_reader(
-        keyspace=keyspace,
-        table_suffix=table_suffix,
-        prefetch_buffers=prefetch_buffers,
-        shard_id=shard_id,
-        num_shards=num_shards,
-        source_uuids=source_uuids,
-        io_threads=io_threads,
-        comm_threads=comm_threads,
-        copy_threads=copy_threads,
-        wait_threads=wait_threads,
-        ooo=True,
-        slow_start=4,
-    )
-    images, labels = cass_reader
-    dali_device = "cpu" if dali_cpu else "gpu"
-    decoder_device = "cpu" if dali_cpu else "mixed"
-    # ask nvJPEG to preallocate memory for the biggest sample in ImageNet for CPU and GPU to avoid reallocations in runtime
-    device_memory_padding = 211025920 if decoder_device == "mixed" else 0
-    host_memory_padding = 140544512 if decoder_device == "mixed" else 0
-    # ask HW NVJPEG to allocate memory ahead for the biggest image in the data set to avoid reallocations in runtime
-    preallocate_width_hint = 5980 if decoder_device == "mixed" else 0
-    preallocate_height_hint = 6430 if decoder_device == "mixed" else 0
-    if is_training:
-        images = fn.decoders.image_random_crop(
-            images,
-            device=decoder_device,
-            output_type=types.RGB,
-            device_memory_padding=device_memory_padding,
-            host_memory_padding=host_memory_padding,
-            preallocate_width_hint=preallocate_width_hint,
-            preallocate_height_hint=preallocate_height_hint,
-            random_aspect_ratio=[0.8, 1.25],
-            random_area=[0.1, 1.0],
-            num_attempts=100,
-        )
-        images = fn.resize(
-            images,
-            device=dali_device,
-            resize_x=crop,
-            resize_y=crop,
-            interp_type=types.INTERP_TRIANGULAR,
-        )
-        mirror = fn.random.coin_flip(probability=0.5)
-    else:
-        images = fn.decoders.image(images, device=decoder_device, output_type=types.RGB)
-        images = fn.resize(
-            images,
-            device=dali_device,
-            size=size,
-            mode="not_smaller",
-            interp_type=types.INTERP_TRIANGULAR,
-        )
-        mirror = False
-
-    images = fn.crop_mirror_normalize(
-        images.gpu(),
-        dtype=types.FLOAT,
-        output_layout="CHW",
-        crop=(crop, crop),
-        mean=[0.485 * 255, 0.456 * 255, 0.406 * 255],
-        std=[0.229 * 255, 0.224 * 255, 0.225 * 255],
-        mirror=mirror,
-    )
-    labels = labels.gpu()
-    return (images, labels)
-
+## Base class: no DALI 
 
 class ImageNetLightningModel(L.LightningModule):
     MODEL_NAMES = sorted(
@@ -340,15 +240,17 @@ class ImageNetLightningModel(L.LightningModule):
     def training_step(self, batch, batch_idx):
         images = batch[0]["data"]
         target = batch[0]["label"].squeeze(-1).long()
+        
+        # Get output from the model
         output = self(images)
 
         #print (f"output_shape: {output.shape}, target_shape: {target.shape}")
 
         loss_train = F.cross_entropy(output, target)
         acc1, acc5 = self.__accuracy(output, target, topk=(1, 5))
-        self.log("train_loss", loss_train, on_step=True, on_epoch=True, logger=True)
-        self.log("train_acc1", acc1, on_step=True, prog_bar=True, on_epoch=True, logger=True)
-        self.log("train_acc5", acc5, on_step=True, on_epoch=True, logger=True)
+        self.log("train_loss", loss_train, prog_bar=True, on_step=False, on_epoch=True, logger=True, sync_dist=True)
+        self.log("train_acc1", acc1, prog_bar=True, on_step=False, on_epoch=True, logger=True, sync_dist=True)
+        self.log("train_acc5", acc5, prog_bar=False, on_step=False, on_epoch=True, logger=True, sync_dist=True)
         return loss_train
 
     def eval_step(self, batch, batch_idx, prefix: str):
@@ -357,9 +259,9 @@ class ImageNetLightningModel(L.LightningModule):
         output = self(images)
         loss_val = F.cross_entropy(output, target)
         acc1, acc5 = self.__accuracy(output, target, topk=(1, 5))
-        self.log(f"{prefix}_loss", loss_val, on_step=True, on_epoch=True)
-        self.log(f"{prefix}_acc1", acc1, on_step=True, prog_bar=True, on_epoch=True)
-        self.log(f"{prefix}_acc5", acc5, on_step=True, on_epoch=True)
+        self.log(f"{prefix}_loss", loss_val, prog_bar=True, on_step=False, on_epoch=True, sync_dist=True, logger=True)
+        self.log(f"{prefix}_acc1", acc1,  prog_bar=True, on_step=False, on_epoch=True, sync_dist=True, logger=True)
+        self.log(f"{prefix}_acc5", acc5, prog_bar=False, on_step=False, on_epoch=True, sync_dist=True, logger=True)
 
     def validation_step(self, batch, batch_idx):
         return self.eval_step(batch, batch_idx, "val")
@@ -382,18 +284,16 @@ class ImageNetLightningModel(L.LightningModule):
             return res
 
     def configure_optimizers(self):
-        # Scale learning rate based on global batch size FIXME
-        # self.lr = self.lr * float(self.batch_size * world_size) / 256.0  
+        # Scale learning rate based on global batch size. 
+        # The Actual batch size in a distributed training setup is muliplied by the number of workers k
+        # Using the linear Scaling Rule (When scaling the batch size by k, scale the learning rate also by k)
+        self.lr = self.lr * self.trainer.world_size
         optimizer = optim.SGD(self.parameters(), lr=self.lr, momentum=self.momentum, weight_decay=self.weight_decay)
         scheduler = lr_scheduler.LambdaLR(optimizer, lambda epoch: 0.1 ** (epoch // 30))
         return [optimizer], [scheduler]
 
-    def on_train_epoch_end(self):
-        self.train_loader.reset()
-    
-    def on_validation_epoch_end(self):
-        self.val_loader.reset()
 
+## Derived class to add the Cassandra DALI pipeline 
 
 class DALI_ImageNetLightningModel(ImageNetLightningModel):
     def __init__(self, 
@@ -403,13 +303,21 @@ class DALI_ImageNetLightningModel(ImageNetLightningModel):
 
     def prepare_data(self):
         # no preparation is needed in DALI
+        # All the preprocessing steps are performed within the DALI pipeline
         pass
 
     def setup(self, stage=None):
+        ## Get info for distributed setup
         device_id = self.local_rank
         shard_id = self.global_rank
         num_shards = self.trainer.world_size
+        
 
+        # Create DALI pipelines (with cassandra plugins)
+        train_pipeline = self.GetPipeline(args, args.train_table_suffix, is_training=True, device_id=device_id, shard_id=shard_id, num_shards=num_shards, num_threads=8)
+        val_pipeline = self.GetPipeline(args, args.val_table_suffix, is_training=False, device_id=device_id, shard_id=shard_id, num_shards=num_shards, num_threads=8)
+
+        # Wrapper class to allow for adding code to the methods
         class LightningWrapper(DALIClassificationIterator):
             def __init__(self, *kargs, **kvargs):
                 super().__init__(*kargs, **kvargs)
@@ -418,11 +326,9 @@ class DALI_ImageNetLightningModel(ImageNetLightningModel):
                 out = super().__next__()
                 return out
 
-        train_pipeline = self.GetPipeline(args, args.train_table_suffix, is_training=True, device_id=device_id, shard_id=shard_id, num_shards=num_shards, num_threads=8)
-        self.train_loader = LightningWrapper(train_pipeline, last_batch_policy=LastBatchPolicy.PARTIAL, reader_name="Reader")
-
-        val_pipeline = self.GetPipeline(args, args.val_table_suffix, is_training=False, device_id=device_id, shard_id=shard_id, num_shards=num_shards, num_threads=8)
-        self.val_loader = LightningWrapper(val_pipeline, last_batch_policy=LastBatchPolicy.PARTIAL, reader_name="Reader")
+        # Creatind actual loaders used by the lightning module to get data (train_dataloader and val_dataloader methods)
+        self.train_loader = LightningWrapper(train_pipeline, last_batch_policy=LastBatchPolicy.PARTIAL, auto_reset=True, reader_name="Reader")
+        self.val_loader = LightningWrapper(val_pipeline, last_batch_policy=LastBatchPolicy.PARTIAL, auto_reset=True, reader_name="Reader")
     
     def train_dataloader(self):
         return self.train_loader
@@ -430,6 +336,12 @@ class DALI_ImageNetLightningModel(ImageNetLightningModel):
     def val_dataloader(self):
         return self.val_loader
    
+    # def on_train_epoch_end(self):
+    #     self.train_loader.reset()
+    # 
+    # def on_validation_epoch_end(self):
+    #     self.val_loader.reset()
+
     @staticmethod
     def GetPipeline(args, table_suffix, is_training, device_id, shard_id, num_shards, num_threads):
         in_uuids = read_uuids(
@@ -469,16 +381,6 @@ def main():
     else:
         profiler = None
 
-    # test mode, use default args for sanity test
-    if args.test:
-        args.opt_level = None
-        args.epochs = 1
-        args.start_epoch = 0
-        args.arch = "resnet50"
-        args.batch_size = 64
-        args.sync_bn = False
-        print("Test mode - no DDP, no apex, RN50, 10 iterations")
-
     print("loss_scale = {}".format(args.loss_scale), type(args.loss_scale))
 
     print("\nCUDNN VERSION: {}\n".format(torch.backends.cudnn.version()))
@@ -492,9 +394,6 @@ def main():
         args.crop_size = 224
         args.val_size = 256
 
-    #if args.evaluate:
-    #    validate(val_loader, model, criterion)
-    #    return
 
     # create lightning model
     model = DALI_ImageNetLightningModel(args)
@@ -527,31 +426,21 @@ def main():
         resume()
         """
 
+    #if args.evaluate:
+    #    validate(val_loader, model, criterion)
+    #    return
+
+    tensorboard_logger = TensorBoardLogger("tb_logs", name="my_model")
+    csv_logger = logger = CSVLogger("logs_csv", name="my_model")
+    logger = csv_logger
 
     ### Lightning Trainer
     if args.num_gpu > 1:
-        trainer = L.Trainer(max_epochs=args.epochs,  accelerator="gpu", devices=args.num_gpu, strategy='ddp', profiler=profiler)
+        trainer = L.Trainer(max_epochs=args.epochs,  accelerator="gpu", devices=args.num_gpu, strategy='ddp', profiler=profiler, num_sanity_val_steps=0, logger=logger)
     else:
-        trainer = L.Trainer(max_epochs=args.epochs,  accelerator="gpu", devices=1, profiler=profiler)
+        trainer = L.Trainer(max_epochs=args.epochs,  accelerator="gpu", devices=1, profiler=profiler, num_sanity_val_steps=0, logger=logger)
     
     trainer.fit(model)
-
-
-def adjust_learning_rate(optimizer, epoch, step, len_epoch):
-    """LR schedule that should yield 76% converged accuracy with batch size 256"""
-    factor = epoch // 30
-
-    if epoch >= 80:
-        factor = factor + 1
-
-    lr = args.lr * (0.1 ** factor)
-
-    """Warmup"""
-    if epoch < 5:
-        lr = lr * float(1 + step + epoch * len_epoch) / (5.0 * len_epoch)
-
-    for param_group in optimizer.param_groups:
-        param_group["lr"] = lr
 
 
 if __name__ == "__main__":
