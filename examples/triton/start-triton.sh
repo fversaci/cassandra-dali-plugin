42fab3bf986e37c40bd007dee47f0c45a2225dec
Francesco Versaci <francesco.versaci@gmail.com>
2024-05-23 17:54:42 +0200

    Adopt tritonserver:24.04-py3 (without conda)

--- examples/triton/start-triton.sh
+++ examples/triton/start-triton.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-tritonserver --model-repository ./models --backend-config dali,plugin_libs=/opt/conda/lib/python3.8/site-packages/libcrs4cassandra.so # --cuda-memory-pool-byte-size 0:134217728 --pinned-memory-pool-byte-size 536870912
+tritonserver --model-repository ./models --backend-config dali,plugin_libs=/usr/local/lib/python3.10/dist-packages/libcrs4cassandra.so
+# --cuda-memory-pool-byte-size 0:134217728 --pinned-memory-pool-byte-size 536870912
