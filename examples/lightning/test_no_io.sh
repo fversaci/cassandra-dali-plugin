c3f74aa943619e6e509cedb1b006abd13cc5f391
Giovanni <giovanni.busonera@crs4.it>
2024-09-10 07:01:19 +0000

    Added scritps to perform imagenet tests with different data loading approach

---
+++ examples/lightning/test_no_io.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+HOST="KENTU"
+BS=512
+MAX_GPUS=`nvidia-smi -L | wc -l`
+EPOCHS=2
+
+
+echo $HOST
+
+### NO Dataloader 
+python3 train_model_no_IO.py --epoch ${EPOCHS} -b ${BS} --no-io -g 1 --log-csv ${HOST}_1_GPU_NO_IO_BS_${BS}
+python3 train_model_no_IO.py --epoch ${EPOCHS} -b ${BS} --no-io -g ${MAX_GPUS} --log-csv ${HOST}_${MAX_GPUS}_GPU_NO_IO_BS_${BS}
