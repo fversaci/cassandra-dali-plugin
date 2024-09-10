f4956485bdfc78cccb7404baa734bebf6734efc0
Giovanni <giovanni.busonera@crs4.it>
2024-09-10 08:09:08 +0000

    added test with files and some minor changes

--- examples/lightning/test_local.sh
+++ examples/lightning/test_local.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 HOST="KENTU"
 BS=512
@@ -8,22 +8,22 @@ EPOCHS=2
 
 echo $HOST
 
-### NO Dataloader 
-python3 train_model_no_IO.py --epoch ${EPOCHS} -b ${BS} --no-io -g 1 --log-csv ${HOST}_1_GPU_NO_IO_BS_${BS}
-python3 train_model_no_IO.py --epoch ${EPOCHS} -b ${BS} --no-io -g ${MAX_GPUS} --log-csv ${HOST}_${MAX_GPUS}_GPU_NO_IO_BS_${BS}
-
 ### Files
-TRAIN_FOLDER=
-VAL_FOLDER=
+TRAIN_FOLDER=/scratch/imagenet-files/train
+VAL_FOLDER=/scratch/imagenet-files/val
+
+
+python3 train_model_no_IO.py --train-folder ${TRAIN_FOLDER} --val-folder ${VAL_FOLDER} -g 1 -b ${BS} --log-csv ${HOST}_1_GPU_TFR_BS_${BS}
+
+python3 train_model_no_IO.py --train-folder ${TRAIN_FOLDER} --val-folder ${VAL_FOLDER} -g ${MAX_GPUS} -b ${BS} --log-csv ${HOST}_${MAX_GPUS}_GPU_TFR_BS_${BS}
 
 
 ### Tfrecords
-### Cassandra
-TRAIN_DATA=/scratch/imagenet-tfrecords/train
+TRAIN_TFR=/scratch/imagenet-tfrecords/train
 TRAIN_INDEX=/scratch/imagenet-tfrecords/train_idx/
-VAL_DATA=/scratch/imagenet-tfrecords/val
+VAL_TFR=/scratch/imagenet-tfrecords/val
 VAL_INDEX=/scratch/imagenet-tfrecords/val_idx/
 
-python3 train_model_no_IO.py --train-tfr-folder ${TRAIN_DATA} --train-index-folder ${TRAIN_INDEX} --val-tfr-folder ${VAL_DATA} --val-index-folder ${VAL_INDEX} -g 1 -b ${BS} --log-csv ${HOST}_1_GPU_TFR_BS_${BS}
+python3 train_model_no_IO.py --train-tfr-folder ${TRAIN_TFR} --train-index-folder ${TRAIN_INDEX} --val-tfr-folder ${VAL_TFR} --val-index-folder ${VAL_INDEX} -g 1 -b ${BS} --log-csv ${HOST}_1_GPU_TFR_BS_${BS}
 
-python3 train_model_no_IO.py --train-tfr-folder ${TRAIN_DATA} --train-index-folder ${TRAIN_INDEX} --val-tfr-folder ${VAL_DATA} --val-index-folder ${VAL_INDEX} -g ${MAX_GPUS} -b ${BS} --log-csv ${HOST}_${MAX_GPUS}_GPU_TFR_BS_${BS}
+python3 train_model_no_IO.py --train-tfr-folder ${TRAIN_TFR} --train-index-folder ${TRAIN_INDEX} --val-tfr-folder ${VAL_TFR} --val-index-folder ${VAL_INDEX} -g ${MAX_GPUS} -b ${BS} --log-csv ${HOST}_${MAX_GPUS}_GPU_TFR_BS_${BS}
