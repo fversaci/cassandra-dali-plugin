d54fb4e7b42167353f87415bd36d96bc98bc9c0a
Giovanni <giovanni.busonera@crs4.it>
2024-09-12 17:40:11 +0000

    bugfix. Epoch parameter was not working properly

--- examples/lightning/test_no_io.fish
+++ examples/lightning/test_no_io.fish
@@ -26,5 +26,5 @@ set EPOCHS $_flag_epochs
 set MAX_GPUS (nvidia-smi -L | wc -l)
 
 ### NO Dataloader
-python3 train_model_no_IO.py --epoch $EPOCHS -b $BS --no-io -g 1 --log-csv "$HOST_1_GPU_NO_IO_BS_$BS"
-python3 train_model_no_IO.py --epoch $EPOCHS -b $BS --no-io -g $MAX_GPUS --log-csv "$HOST_$MAX_GPUS_GPU_NO_IO_BS_$BS"
+python3 train_model_no_IO.py --epoch $EPOCHS -b $BS --no-io -g 1 --log-csv "$HOST"_1_GPU_NO_IO_BS_"$BS"
+python3 train_model_no_IO.py --epoch $EPOCHS -b $BS --no-io -g $MAX_GPUS --log-csv "$HOST"_"$MAX_GPUS"_GPU_NO_IO_BS_"$BS"
