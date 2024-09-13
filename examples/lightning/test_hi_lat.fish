95dcfffc738140f448dcf6ef0b037135df67ce47
Giovanni <giovanni.busonera@crs4.it>
2024-09-13 16:50:51 +0000

    test scripts compliant to the changes of cached IDs management

--- examples/lightning/test_hi_lat.fish
+++ examples/lightning/test_hi_lat.fish
@@ -37,43 +37,48 @@ echo $HOST
 
 ### Hi-latency Tests
 
-### CASSANDRA
-
 set TRAIN_DATA imagenet.data_train
 set TRAIN_METADATA imagenet.metadata_train
+set TRAIN_ROWS train.rows
 set VAL_DATA imagenet.data_val
 set VAL_METADATA imagenet.metadata_val
+SET VAL_ROWS val.rows
+
+### CASSANDRA
+
 set IO_THREADS 4
 set PREFETCH_BUFF 2
 set CASSANDRA_IP $_flag_ipc
 
 sed -i --follow-symlinks "/cassandra_ips/s/\(\[.*\]\)/\[\"$CASSANDRA_IP\"\]/" private_data.py
 
-rm -f ids_cache/*
-python3 cache_uuids.py --metadata-table=imagenet.metadata_train
-python3 cache_uuids.py --metadata-table=imagenet.metadata_val
+rm $TRAIN_ROWS
+rm $VAL_ROWS
+python3 cache_uuids.py --metadata-table=$TRAIN_METADATA --rows-fn=$TRAIN_ROWS
+python3 cache_uuids.py --metadata-table=$VAL_METADATA --rows-fn=$VAL_ROWS
 
-python3 train_model_no_IO.py --epoch $EPOCHS --train-data-table $TRAIN_DATA --train-metadata-table $TRAIN_METADATA --val-data-table $VAL_DATA --val-metadata-table $VAL_METADATA --n-io-threads $IO_THREADS --n-prefetch-buffers $PREFETCH_BUFF -g 1 -b $BS --log-csv "$HOST"_1_GPU_CASSANDRA_BS_"$BS"
+echo "CASSANDRA TEST"
 
-python3 train_model_no_IO.py --epoch $EPOCHS --train-data-table $TRAIN_DATA --train-metadata-table $TRAIN_METADATA --val-data-table $VAL_DATA --val-metadata-table $VAL_METADATA --n-io-threads $IO_THREADS --n-prefetch-buffers $PREFETCH_BUFF -g $MAX_GPUS -b $BS --log-csv "$HOST"_"$MAX_GPUS"_GPU_CASSANDRA_BS_"$BS"
+python3 train_model.py --epoch $EPOCHS --train-data-table $TRAIN_DATA --train-rows-fn $TRAIN_ROWS --val-data-table $VAL_DATA --val-rows-fn $VAL_ROWS --n-io-threads $IO_THREADS --n-prefetch-buffers $PREFETCH_BUFF -g 1 -b $BS --log-csv "$HOST"_1_GPU_CASSANDRA_BS_"$BS"
+
+python3 train_model.py --epoch $EPOCHS --train-data-table $TRAIN_DATA --train-rows-fn $TRAIN_ROWS --val-data-table $VAL_DATA --val-rows-fn $VAL_ROWS --n-io-threads $IO_THREADS --n-prefetch-buffers $PREFETCH_BUFF -g $MAX_GPUS -b $BS --log-csv "$HOST"_"$MAX_GPUS"_GPU_CASSANDRA_BS_"$BS"
 
 
 ### SCYLLA
 
-set TRAIN_DATA imagenet.data_train
-set TRAIN_METADATA imagenet.metadata_train
-set VAL_DATA imagenet.data_val
-set VAL_METADATA imagenet.metadata_val
 set IO_THREADS 4
 set PREFETCH_BUFF 2
 set SCYLLA_IP $_flag_ips
 
 sed -i --follow-symlinks "/cassandra_ips/s/\(\[.*\]\)/\[\"$SCYLLA_IP\"\]/" private_data.py
 
-rm -f ids_cache/*
-python3 cache_uuids.py --metadata-table=imagenet.metadata_train
-python3 cache_uuids.py --metadata-table=imagenet.metadata_val
+rm $TRAIN_ROWS
+rm $VAL_ROWS
+python3 cache_uuids.py --metadata-table=$TRAIN_METADATA --rows-fn=$TRAIN_ROWS
+python3 cache_uuids.py --metadata-table=$VAL_METADATA --rows-fn=$VAL_ROWS
 
-python3 train_model_no_IO.py --epoch $EPOCHS --train-data-table $TRAIN_DATA --train-metadata-table $TRAIN_METADATA --val-data-table $VAL_DATA --val-metadata-table $VAL_METADATA --n-io-threads $IO_THREADS --n-prefetch-buffers $PREFETCH_BUFF -g 1 -b $BS --log-csv "$HOST"_1_GPU_SCYLLA_BS_"$BS"
+echo "SCYLLA TEST"
 
-python3 train_model_no_IO.py --epoch $EPOCHS --train-data-table $TRAIN_DATA --train-metadata-table $TRAIN_METADATA --val-data-table $VAL_DATA --val-metadata-table $VAL_METADATA --n-io-threads $IO_THREADS --n-prefetch-buffers $PREFETCH_BUFF -g $MAX_GPUS -b $BS --log-csv "$HOST"_"$MAX_GPUS"_GPU_SCYLLA_BS_"$BS"
+python3 train_model.py --epoch $EPOCHS --train-data-table $TRAIN_DATA --train-rows-fn $TRAIN_ROWS --val-data-table $VAL_DATA --val-rows-fn $VAL_ROWS --n-io-threads $IO_THREADS --n-prefetch-buffers $PREFETCH_BUFF -g 1 -b $BS --log-csv "$HOST"_1_GPU_SCYLLA_BS_"$BS"
+
+python3 train_model.py --epoch $EPOCHS --train-data-table $TRAIN_DATA --train-rows-fn $TRAIN_ROWS --val-data-table $VAL_DATA --val-rows-fn $VAL_ROWS --n-io-threads $IO_THREADS --n-prefetch-buffers $PREFETCH_BUFF -g $MAX_GPUS -b $BS --log-csv "$HOST"_"$MAX_GPUS"_GPU_SCYLLA_BS_"$BS"
--- examples/lightning/test_local.fish
+++ examples/lightning/test_local.fish
@@ -29,9 +29,9 @@ set MAX_GPUS (nvidia-smi -L | wc -l)
 set TRAIN_FOLDER /scratch/imagenet-files/train
 set VAL_FOLDER /scratch/imagenet-files/val
 
-python3 train_model_no_IO.py --epoch $EPOCHS --train-folder $TRAIN_FOLDER --val-folder $VAL_FOLDER -g 1 -b $BS --log-csv "$HOST"_1_GPU_TFR_BS_"$BS"
+python3 train_model.py --epoch $EPOCHS --train-folder $TRAIN_FOLDER --val-folder $VAL_FOLDER -g 1 -b $BS --log-csv "$HOST"_1_GPU_TFR_BS_"$BS"
 
-python3 train_model_no_IO.py --epoch $EPOCHS --train-folder $TRAIN_FOLDER --val-folder $VAL_FOLDER -g $MAX_GPUS -b $BS --log-csv "$HOST"_"$MAX_GPUS"_GPU_TFR_BS_"$BS"
+python3 train_model.py --epoch $EPOCHS --train-folder $TRAIN_FOLDER --val-folder $VAL_FOLDER -g $MAX_GPUS -b $BS --log-csv "$HOST"_"$MAX_GPUS"_GPU_TFR_BS_"$BS"
 
 ### Tfrecords
 set TRAIN_TFR /scratch/imagenet-tfrecords/train
@@ -39,7 +39,7 @@ set TRAIN_INDEX /scratch/imagenet-tfrecords/train_idx/
 set VAL_TFR /scratch/imagenet-tfrecords/val
 set VAL_INDEX /scratch/imagenet-tfrecords/val_idx/
 
-python3 train_model_no_IO.py --epoch $EPOCHS --train-tfr-folder $TRAIN_TFR --train-index-folder $TRAIN_INDEX --val-tfr-folder $VAL_TFR --val-index-folder $VAL_INDEX -g 1 -b $BS --log-csv "$HOST"_1_GPU_TFR_BS_"$BS"
+python3 train_model.py --epoch $EPOCHS --train-tfr-folder $TRAIN_TFR --train-index-folder $TRAIN_INDEX --val-tfr-folder $VAL_TFR --val-index-folder $VAL_INDEX -g 1 -b $BS --log-csv "$HOST"_1_GPU_TFR_BS_"$BS"
 
-python3 train_model_no_IO.py --epoch $EPOCHS --train-tfr-folder $TRAIN_TFR --train-index-folder $TRAIN_INDEX --val-tfr-folder $VAL_TFR --val-index-folder $VAL_INDEX -g $MAX_GPUS -b $BS --log-csv "$HOST"_"$MAX_GPUS"_GPU_TFR_BS_"$BS"
+python3 train_model.py --epoch $EPOCHS --train-tfr-folder $TRAIN_TFR --train-index-folder $TRAIN_INDEX --val-tfr-folder $VAL_TFR --val-index-folder $VAL_INDEX -g $MAX_GPUS -b $BS --log-csv "$HOST"_"$MAX_GPUS"_GPU_TFR_BS_"$BS"
 
