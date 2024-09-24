f33dea22d39d4e10fdc5d9bf0f63b4d3c4997f1a
Giovanni <giovanni.busonera@crs4.it>
2024-09-24 16:51:48 +0000

    Implemented an S3 dataset, allowing the dataloader to seamlessly handle both S3 and local files

--- examples/imagenette/pytorch_loop_read.py
+++ examples/imagenette/pytorch_loop_read.py
@@ -1,4 +1,4 @@
-import os
+import os, sys
 import statistics
 from clize import run
 from PIL import Image
@@ -9,9 +9,88 @@ from tqdm import tqdm, trange
 from torchdata.datapipes.iter import IterableWrapper
 from IPython import embed
 
+import boto3
 import time
 import numpy as np
 import pickle
+
+
+def parse_s3_uri(s3_uri):
+    if not s3_uri.startswith("s3://"):
+        raise ValueError("Invalid S3 URI")
+
+    # Remove the "s3://" prefix
+    s3_uri = s3_uri[5:]
+
+    # Split the remaining part into bucket and prefix
+    parts = s3_uri.split("/", 1)
+    bucket_name = parts[0]
+    prefix = parts[1] if len(parts) > 1 else ""
+
+    return bucket_name, prefix
+
+
+def list_s3_files(s3_uri):
+    bucket_name, prefix = parse_s3_uri(s3_uri)
+    s3 = boto3.client("s3")
+    paginator = s3.get_paginator("list_objects_v2")
+    pages = paginator.paginate(Bucket=bucket_name, Prefix=prefix)
+
+    paths = []
+    for page in pages:
+        if "Contents" in page:
+            for obj in page["Contents"]:
+                paths.append(f"s3://{bucket_name}/{obj['Key']}")
+
+    return sorted(paths)
+
+
+class S3Dataset(Dataset):
+    def __init__(self, s3_root_dir, transform=None):
+        self.s3 = boto3.client('s3')
+        self.s3_bucket, self.prefix = parse_s3_uri(s3_root_dir)
+        self.file_keys = self.get_file_list(s3_root_dir) # List of keys (file names/paths) in the S3 bucket
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.file_keys)
+
+    def __getitem__(self, idx):
+        fname = self.file_keys[idx][0]
+        label = self.file_keys[idx][1]
+        
+        # Read the file from S3
+        response = self.s3.get_object(Bucket=self.s3_bucket, Key=fname)
+        
+        image = response['Body'].read()
+
+        if self.transform:
+            image = self.transform(image)
+
+        return image, label
+    
+    def get_file_list(self, root_dir): 
+        file_keys = list_s3_files(root_dir)
+        
+        label_index = 0
+        label_map = {}
+
+        s3_files = []
+        
+        for file in file_keys:
+            els = file.split('/')[-2:]
+            label_name = els[0]
+            basename = els[1]
+
+            if label_name not in label_map:
+                label_map[label_name] = label_index
+                s3_files.append((os.path.join(self.prefix, '/'.join([label_name, basename])), label_index))
+                label_index += 1
+            
+            else:
+                s3_files.append((os.path.join(self.prefix, '/'.join([label_name, basename])), label_map[label_name]))
+            
+        return s3_files
 
 
 class ImageNetDataset(Dataset):
@@ -39,41 +118,31 @@ class ImageNetDataset(Dataset):
         return img, label
 
 
-def custom_collate(batch):
-    return batch
-
-
 def scan(*, root_dir="s3://imagenet/val", epochs=4, bs=128, log_fn=None):
     if "s3://" in root_dir:
         # Create pytorch file loader from s3
         print("Reading from S3")
-        s3_shard_urls = IterableWrapper(
-            [
-                root_dir,
-            ]
-        ).list_files_by_s3()
-        s3_files = s3_shard_urls.sharding_filter().load_files_by_s3()
-        # tf_records = s3_files.load_from_tfrecord()
-        # text data
+        #steps = int(np.ceil(len(list(s3_shard_urls)) / bs))        
+        
+        s3_dataset = S3Dataset(root_dir)
+        
         data_loader = DataLoader(
-            s3_files,
+            s3_dataset,
             batch_size=bs,
             num_workers=32,
             prefetch_factor=8,
-            collate_fn=custom_collate,
             shuffle=True,
         )
-
     else:
         # Create pytorch file loader from filesystem
         print("Reading from a local filesystem")
         dataset = ImageNetDataset(root_dir)
         data_loader = DataLoader(
-            dataset, batch_size=bs, shuffle=True, num_workers=2, prefetch_factor=8
-        )
+            dataset, batch_size=bs, shuffle=True, num_workers=2, prefetch_factor=8)
+    
+    steps = len(data_loader)
 
     ### Start scanning
-    steps = len(data_loader)
     print(f"Batches per epoch: {steps}")
 
     timestamps_np = np.zeros((epochs, steps))
@@ -88,7 +157,7 @@ def scan(*, root_dir="s3://imagenet/val", epochs=4, bs=128, log_fn=None):
             for step, b in enumerate(t):
                 images = b[0]
                 labels = b[1]
-
+                
                 images_batch_bytes = sum([len(dd) for dd in images])
                 labels_batch_bytes = labels.shape[0]
                 batch_bytes = images_batch_bytes + labels_batch_bytes
@@ -96,7 +165,8 @@ def scan(*, root_dir="s3://imagenet/val", epochs=4, bs=128, log_fn=None):
                 batch_bytes_np[epoch][step] = batch_bytes
                 timestamps_np[epoch, step] = time.time() - start_ts
                 start_ts = time.time()
-
+                
+                
     # Calculate the average and standard deviation
     if epochs > 3:
         # First epoch is skipped
