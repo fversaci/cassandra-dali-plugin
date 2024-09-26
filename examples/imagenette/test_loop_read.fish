47bb21d435f038b88318d885f7b56d7e9764c9f6
Francesco Versaci <francesco.versaci@gmail.com>
2024-09-26 14:14:08 +0000

    unify IP's + varia

--- examples/imagenette/test_loop_read.fish
+++ examples/imagenette/test_loop_read.fish
@@ -1,14 +1,27 @@
 #!/usr/bin/env fish
 
+
 function usage
-    echo "Usage: script_name.fish --host <host> --bs <batch size> --epochs <num_epochs> --ipc <ip_cassandra> --ips <ip_scylla> --ipm <ip_s3>"
+    echo "Usage: script_name.fish --host <host> --bs <batch size> --epochs <num_epochs> --ip <ip_server> --pc <port_cassandra> --ps <port_scylla> --rootdir <root data dir> --logdir <log dir> --debug"
 end
 
 # Parse arguments
-argparse 'h/host=' 'b/bs=' 'e/epochs=' 'c/ipc=' 's/ips=' 'm/ipm' -- $argv
+argparse 'h/host=' 'bs=' 'e/epochs=' 'ip=' 'pc=' 'ps=' 'd/rootdir=' 'logdir=' 'debug' -- $argv
+
+if set -q _flag_debug
+    set fish_trace 1
+end
 
 if not set -q _flag_host
     set _flag_host NONE
+end
+
+if not set -q _flag_rootdir
+    set _flag_rootdir "/data"
+end
+
+if not set -q _flag_logdir
+    set _flag_logdir "log"
 end
 
 if not set -q _flag_bs
@@ -19,83 +32,81 @@ if not set -q _flag_epochs
     set _flag_epochs 10
 end
 
-if not set -q _flag_ipc
-    set _flag_ipc 172.19.179.85
+if not set -q _flag_ip
+    set _flag_ip 172.19.179.86
 end
 
-if not set -q _flag_ips
-    set _flag_ips 172.19.179.86
+if not set -q _flag_pc
+    set _flag_pc 9042
 end
 
-if not set -q _flag_ipm
-    set _flag_ipm 172.19.179.86
+if not set -q _flag_ps
+    set _flag_ps 9043
 end
 
 # Access the values passed to the named parameters
 set HOST $_flag_host
 set BS $_flag_bs
 set EPOCHS $_flag_epochs
+set ROOT $_flag_rootdir
+set LOG $_flag_logdir
+# create log dir
+mkdir -p $LOG
 
-echo $HOST
-echo $_flag_ipc
-echo $_flag_ips
-
-### Set environment variables to test S3
-set S3_IP $_flag_ipm
+# Set environment variables to test S3
+set S3_IP $_flag_ip
+set S3_PORT 9000
 set -e http_proxy
-set -x AWS_ENDPOINT_URL "http://$S3_IP:9000"
+set -x AWS_ENDPOINT_URL "http://$S3_IP:$S3_PORT"
 set -x AWS_ACCESS_KEY_ID root
 set -x AWS_SECRET_ACCESS_KEY passpass
-set -x S3_ENDPOINT_URL "http://$S3_IP:9000"
+set -x S3_ENDPOINT_URL "http://$S3_IP:$S3_PORT"
 
-### Local filesystem
- ## file with Pytorch
-
-  ## file with DALI
-python loop_read.py --epochs 10 --bs 1024 --reader file --file-root /scratch/imagenet-files/train/ --log-fn "$HOST"_loop_read_DALI_file_BS_"$BS".pickle
+## Local filesystem
+### files with DALI
+echo "-- DALI FILES TEST --"
+python loop_read.py --epochs $EPOCHS --bs $BS --reader file --file-root $ROOT/imagenet-files/train/ --log-fn "$LOG/$HOST"_loop_read_DALI_file_BS_"$BS".pickle
 	
-  ## TfRecord with DALI
-python loop_read.py --epochs 10 --bs 1024 --reader tfrecord --file-root /scratch/imagenet-tfrecords/train/ --index-root /scratch/imagenet-tfrecords/train_idx/ --log-fn "$HOST"_loop_read_DALI_tfrecord_BS_"$BS".pickle
+### TFRecords with DALI
+echo "-- DALI TFRECORDS TEST --"
+python loop_read.py --epochs $EPOCHS --bs $BS --reader tfrecord --file-root $ROOT/imagenet-tfrecords/train/ --index-root $ROOT/imagenet-tfrecords/train_idx/ --log-fn "$LOG/$HOST"_loop_read_DALI_tfrecord_BS_"$BS".pickle
 
-  ## TfRecord with DALI 
+### files with Pytorch
 
 
-### Remote testing 
+## Hi latency
 set TRAIN_DATA imagenet.data_train
 set TRAIN_METADATA imagenet.metadata_train
 set TRAIN_ROWS train.rows
-set VAL_DATA imagenet.data_val
-set VAL_METADATA imagenet.metadata_val
-set VAL_ROWS val.rows
-
-### Medium latency
-
-
-### Hi latency
-set IO_THREADS 4
-set PREFETCH_BUFF 2
-set CASSANDRA_IP $_flag_ipc
-
-sed -i --follow-symlinks "/cassandra_ips/s/\(\[.*\]\)/\[\"$CASSANDRA_IP\"\]/" private_data.py
-
-rm $TRAIN_ROWS
-rm $VAL_ROWS
-python3 cache_uuids.py --metadata-table=$TRAIN_METADATA --rows-fn=$TRAIN_ROWS
-python3 cache_uuids.py --metadata-table=$VAL_METADATA --rows-fn=$VAL_ROWS
-
-echo "CASSANDRA TEST"
 
 ### SCYLLA
-
+echo "-- SCYLLA TEST --"
 set IO_THREADS 4
 set PREFETCH_BUFF 2
-set SCYLLA_IP $_flag_ips
+set SCYLLA_IP $_flag_ip
+set SCYLLA_PORT $_flag_ps
+echo $SCYLLA_PORT
 
 sed -i --follow-symlinks "/cassandra_ips/s/\(\[.*\]\)/\[\"$SCYLLA_IP\"\]/" private_data.py
+sed -i --follow-symlinks "/cassandra_port/s/= \(.*\)/= $SCYLLA_PORT/" private_data.py
 
-rm $TRAIN_ROWS
-rm $VAL_ROWS
+rm -f $TRAIN_ROWS
 python3 cache_uuids.py --metadata-table=$TRAIN_METADATA --rows-fn=$TRAIN_ROWS
-python3 cache_uuids.py --metadata-table=$VAL_METADATA --rows-fn=$VAL_ROWS
+python3 loop_read.py --bs=$BS --epochs=$EPOCHS --data-table=$TRAIN_DATA --rows-fn=$TRAIN_ROWS --log-fn "$LOG/$HOST"_loop_read_scylla_BS_"$BS".pickle
 
-echo "SCYLLA TEST"
+### CASSANDRA
+echo "-- CASSANDRA TEST --"
+set IO_THREADS 4
+set PREFETCH_BUFF 2
+set CASSANDRA_IP $_flag_ip
+set CASSANDRA_PORT $_flag_pc
+
+sed -i --follow-symlinks "/cassandra_ips/s/\(\[.*\]\)/\[\"$CASSANDRA_IP\"\]/" private_data.py
+sed -i --follow-symlinks "/cassandra_port/s/= \(.*\)/= $CASSANDRA_PORT/" private_data.py
+
+rm -f $TRAIN_ROWS
+python3 cache_uuids.py --metadata-table=$TRAIN_METADATA --rows-fn=$TRAIN_ROWS
+python3 loop_read.py --bs=$BS --epochs=$EPOCHS --data-table=$TRAIN_DATA --rows-fn=$TRAIN_ROWS --log-fn "$LOG/$HOST"_loop_read_cassandra_BS_"$BS".pickle
+
+# disable debug print
+set -e fish_trace
