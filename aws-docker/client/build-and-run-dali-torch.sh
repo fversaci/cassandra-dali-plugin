017624d0ff6470affc07d78467ca0e9b2b136cfb
Francesco Versaci <francesco.versaci@gmail.com>
2024-10-29 15:46:59 +0100

    Increase shared memory

--- aws-docker/client/build-and-run-dali-torch.sh
+++ aws-docker/client/build-and-run-dali-torch.sh
@@ -1,7 +1,7 @@
 docker build --progress=plain -t dali:aws -f Dockerfile.dali .
 
 docker run \
-    --cap-add=sys_admin --cap-add=net_admin --shm-size 70GB \
+    --cap-add=sys_admin --cap-add=net_admin --shm-size 200GB \
     --rm -it \
     --ipc=host --ulimit memlock=-1 --ulimit stack=67108864 \
     -v /mnt/scratch/ldic-varia/:/data:rw \
