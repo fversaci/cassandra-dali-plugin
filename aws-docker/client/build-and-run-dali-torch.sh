a8d27fd5a7474d635f07af304655fb7f038cd8d7
Giovanni <giovanni.busonera@crs4.it>
2024-11-05 15:55:36 +0100

    add gpus option and a mount point for the test log folder

--- aws-docker/client/build-and-run-dali-torch.sh
+++ aws-docker/client/build-and-run-dali-torch.sh
@@ -4,8 +4,10 @@ docker build --progress=plain -t dali:aws -f Dockerfile.dali . ; \
 and docker run \
     --cap-add=sys_admin --cap-add=net_admin --shm-size 200GB \
     --rm -it \
+    --gpus=all \
     --ipc=host --ulimit memlock=-1 --ulimit stack=67108864 \
-    -v /mnt/scratch/ldic-varia/:/data:rw \
+    -v /SCRATCH/imagenet/paper/:/data:rw \
+    -v /SCRATCH/imagenet/paper/logs:/logs:rw \
     # -v /mnt/bla:/ebs:rw \
     --entrypoint fish \
     --name client dali:aws
