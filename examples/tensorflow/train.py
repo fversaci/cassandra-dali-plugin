aa4c7e34a78ed8647316a18a65370f8b66409602
Giovanni Busonera <busonera@crs4.it>
2024-11-07 16:03:27 +0000

    Add timestamp logger

--- examples/tensorflow/train.py
+++ examples/tensorflow/train.py
@@ -5,17 +5,33 @@ from tensorflow.keras.applications import ResNet50
 from tqdm import tqdm
 import tensorflow as tf
 from tensorflow.keras import mixed_precision
+import time
 
+from tensorflow.keras.callbacks import Callback
+
+class BatchEpochTimeLogger(Callback):
+    def __init__(self, log_fd):
+        super().__init__()
+        self.fd = log_fd
+
+    def on_epoch_begin(self, epoch, logs=None):
+        self.epoch = epoch
+        self.fd.write("epoch,step,train_acc1,train_acc5,train_batch_ts,train_loss,val_acc1,val_acc5,val_batch_ts,val_loss\n")
+
+    def on_train_batch_begin(self, batch, logs=None):
+        ts = time.time()
+        self.fd.write(f"{self.epoch},{batch},,,{ts},,,,,\n")
+    
 
 def preprocess_image(image):
     image = tf.image.decode_jpeg(image, channels=3)
     image = tf.image.resize(image, [224, 224])
-    # image = tf.keras.applications.resnet50.preprocess_input(image)
-    image = tf.keras.applications.imagenet_utils.preprocess_input(image, mode="torch")
+    image = tf.keras.applications.resnet50.preprocess_input(image)
+    #image = tf.keras.applications.imagenet_utils.preprocess_input(image, mode="torch")
     return image
 
 
-def train(*, bs=128, shuffle_batches=16, tfr=False):
+def train(*, bs=1024, epochs=4, shuffle_batches=16, tfr=False, log_fn=None):
     # Choose if files or tfrecords
     if tfr:
         dataset_id = "imagenet_tfr"
@@ -60,8 +76,16 @@ def train(*, bs=128, shuffle_batches=16, tfr=False):
             metrics=["accuracy"],
         )
 
-    # Train the model
-    model.fit(dataset, epochs=10)
+    if log_fn:
+        fd = open(log_fn, 'w')
+        time_logger = BatchEpochTimeLogger(fd)
+        print ("Start training with log")
+        model.fit(dataset, epochs=epochs, callbacks=[time_logger])
+        fd.close()
+    else:
+        # Train the model
+        print ("Start training")
+        model.fit(dataset, epochs=epochs)
 
 
 if __name__ == "__main__":
