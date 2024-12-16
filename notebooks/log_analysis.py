0bae7cc821dc21da25ccda341963e775e4360fea
Giovanni Busonera <busonera@crs4.it>
2024-12-17 00:13:56 +0100

    will update notebooks

--- notebooks/log_analysis.py
+++ notebooks/log_analysis.py
@@ -19,13 +19,14 @@ import numpy as np
 import yaml
 import matplotlib.pyplot as plt
 from tqdm.notebook import tqdm
+from scipy.stats import hmean
 
-# ls '/home/giovanni/AWS_MAIN_TEST_HI_LAT/train/torch/training/'
+# ls './AWS_logs/AWS_MAIN_TEST_HI_LAT/train/torch/training/'
 
 fnames = []
 
 #folder = "../examples/lightning/logs_csv/ACCA400_1_GPU_NO_IO_BS_1024/version_0/"
-folder = "/home/giovanni/AWS_MAIN_TEST_HI_LAT/train/torch/training/AWS_1_GPU_NO_IO_BS_512/version_0/"
+folder = "./AWS_logs/AWS_MAIN_TEST_HI_LAT/train/torch/training/AWS_1_GPU_NO_IO_BS_512/version_0/"
 hyperparams_fname = os.path.join(folder, "hparams.yaml")
 log_fname = os.path.join(folder, "metrics.csv")
 
@@ -169,43 +170,57 @@ def read_torch_lightning_results(folder):
     else:
         test_name = test_type + "_" + "_".join(log_fname.split("/")[-3].split("_")[3:-2])
     
+    
     log_df = pd.read_csv(log_fname)
+    ## Remove all rows with nan epoch (end of epoch info)
+    log_df  = log_df [~np.isnan(log_df .epoch)]
     log_df["train_batch_ts_diff"] = log_df["train_batch_ts"].diff()
     log_df["val_batch_ts_diff"] = log_df["val_batch_ts"].diff()
     log_df["train_im_sec"] =  (batch_size / log_df["train_batch_ts_diff"]) * num_gpu
     log_df["val_im_sec"] =  (batch_size / log_df["val_batch_ts_diff"]) * num_gpu
     
+    
+    log_df["epoch"] = log_df["epoch"].astype(np.int32) 
     return test_name, num_gpu, batch_size, hyperparams, log_df
 
 
 def read_tf_results(log_fname, batch_size=512, num_gpu=8):
-    test_type = log_fname.split('AWS_MAIN_TEST_')[1].split('_')[0]
-    test_name = test_type + "_" + "_".join(log_fname.split("/")[-1].split("_")[2:-3])
+    
+    if "NO_IO" in log_fname:
+        test_name = "_".join(log_fname.split("/")[-1].split("_")[1:-2])
+        num_gpu = int(log_fname.split("/")[-1].split("_")[1])
+    else:
+        test_type = log_fname.split('AWS_MAIN_TEST_')[1].split('_')[0]
+        test_name = test_type + "_" + "_".join(log_fname.split("/")[-1].split("_")[2:-3])
+    
     log_df = pd.read_csv(log_fname)
+    ## Remove all rows with nan epoch (end of epoch info)
+    log_df  = log_df [~np.isnan(log_df .epoch)]
     log_df["train_batch_ts_diff"] = log_df["train_batch_ts"].diff()
     log_df["val_batch_ts_diff"] = log_df["val_batch_ts"].diff()
     log_df["train_im_sec"] =  (batch_size / log_df["train_batch_ts_diff"]) * num_gpu
     log_df["val_im_sec"] =  (batch_size / log_df["val_batch_ts_diff"]) * num_gpu
-    
+    log_df["epoch"] = log_df["epoch"].astype(np.int32)
     return test_name, log_df
 
 
 # +
-parent = '/home/giovanni/AWS_MAIN_TEST_LOW_LAT/torch/training/'
+parent = './AWS_logs/AWS_MAIN_TEST_LOW_LAT/torch/training/'
 folders = glob.glob(parent + '/*/version_0/')
 
-parent = '/home/giovanni/AWS_MAIN_TEST_MED_LAT/torch/training/'
+parent = './AWS_logs/AWS_MAIN_TEST_MED_LAT/torch/training/'
 folders += glob.glob(parent + '/*/version_0/')
 
-parent = '/home/giovanni/AWS_MAIN_TEST_HI_LAT/train/torch/training/'
+parent = './AWS_logs/AWS_MAIN_TEST_HI_LAT/train/torch/training/'
 folders += glob.glob(parent + '/*/version_0/')
 
 torch_folders = sorted(folders)
 # -
 
-tf_files = ['/home/giovanni/AWS_MAIN_TEST_LOW_LAT/tensorflow/train/AWS_Train_TF_tfdataservice_tfr_BS_512', 
-            '/home/giovanni/AWS_MAIN_TEST_MED_LAT/tensorflow/train/AWS_Train_TF_tfdataservice_tfr_BS_512',
-            '/home/giovanni/AWS_MAIN_TEST_HI_LAT/train/tensorflow/train/AWS_Train_TF_tfdataservice_tfr_BS_512']
+tf_files = ['./AWS_logs/AWS_MAIN_TEST_LOW_LAT/tensorflow/train/AWS_Train_TF_tfdataservice_tfr_BS_512', 
+            './AWS_logs/AWS_MAIN_TEST_MED_LAT/tensorflow/train/AWS_Train_TF_tfdataservice_tfr_BS_512',
+            './AWS_logs/AWS_MAIN_TEST_HI_LAT/train/tensorflow/train/AWS_Train_TF_tfdataservice_tfr_BS_512', 
+            './AWS_logs/AWS_MAIN_TEST_HI_LAT/train/tensorflow/train/AWS_1_GPU_TF_NO_IO_BS_512',]
 
 # +
 data_dict = {}
@@ -213,37 +228,53 @@ data_dict = {}
 ## Get torch data, num gpu and batch size
 for folder in tqdm(torch_folders):
     test_name, num_gpu, batch_size, hyperparams, log_df = read_torch_lightning_results(folder)
-    data_dict[test_name] = (log_df, batch_size, num_gpu)
+    df_group = log_df.groupby("epoch")
+    data_dict[test_name] = (log_df, batch_size, num_gpu, df_group)
 
 ## Get TF data
 for fname in tqdm(tf_files):
     test_name, log_df = read_tf_results(fname)
+    df_group = log_df.groupby("epoch")
     print (test_name)
-    data_dict[test_name] = (log_df, 512, 8)
+    data_dict[test_name] = (log_df, 512, 8, df_group)
 # -
 
 data_dict.keys()
 
+tmp_df = data_dict[list(data_dict.keys())[0]][0]
+tmp_df
+
 # +
 n_subplots = len(data_dict.keys())
 subplot_size = 4
-f, axs = plt.subplots(n_subplots, 1, figsize=(subplot_size*2, n_subplots*subplot_size))
+
 
 for i, test_name in enumerate(data_dict):    
-    df, batch_size, num_gpu = data_dict[test_name]
-
-    x = df["train_batch_ts"] - df["train_batch_ts"].iloc[0]
-    y = df["train_im_sec"]
-    m = np.nanmean(y)
+    df, batch_size, num_gpu, df_group = data_dict[test_name]
     
-    ax = axs[i] 
-    ax.plot(x, y, label=test_name)
-    ax.axhline(m, c='r', ls='--', label=f"mean: {m:.1f}im/sec")
-    ax.set_xlabel("time (s)")
-    ax.set_ylabel("im/sec")
+    n_epochs = len(df_group.groups)
+    
+    f, axs = plt.subplots(1, n_epochs, figsize=(subplot_size*n_epochs*2, subplot_size))
+    
+    for grp in df_group.groups:
+        
+        df = df_group.get_group(grp)
+        if n_epochs == 1:
+            ax = axs
+        else:
+            ax = axs[grp]
 
-    ax.set_ylim(0, 12000)
-    ax.legend()
+        x = df["train_batch_ts"] - df["train_batch_ts"].iloc[0]
+        y = df["train_im_sec"]
+        m = hmean(y[~np.isnan(y)])
+
+        ax.plot(x, y, label=test_name)
+        ax.axhline(m, c='r', ls='--', label=f"mean: {m:.1f}im/sec")
+        ax.set_xlabel("time (s)")
+        ax.set_ylabel("im/sec")
+
+        ax.set_ylim(0, 12000)
+        ax.legend()
 
 plt.tight_layout()
 
@@ -251,12 +282,6 @@ f.savefig("training_timeseries.png", dpi=300)
 # -
 
 
-for i, test_name in enumerate(data_dict):    
-    df, batch_size, num_gpu = data_dict[test_name]
-
-    x = df["train_batch_ts"] - df["train_batch_ts"].iloc[0]
-    y = df["train_im_sec"]
-    m = np.nanmean(y)
-    s = np.nanstd(y)
+# ### Fig 4
 
 
--- notebooks/log_loop_read_analysis.py
+++ notebooks/log_loop_read_analysis.py
@@ -8,9 +8,9 @@
 #       format_version: '1.5'
 #       jupytext_version: 1.16.4
 #   kernelspec:
-#     display_name: Python 3 (ipykernel)
+#     display_name: python3_stats-ML
 #     language: python
-#     name: python3
+#     name: python3_stats-ml
 # ---
 
 import numpy as np
@@ -25,9 +25,23 @@ import pandas as pd
 
 
 # +
-def load_loopread_csv(fn):
+def get_conf_interval_hmean(data, interval=95):
+    l_perc = (100.0-interval) / 2
+    h_perc = 100 - ((100.0-interval) / 2)
+    
+    #print ("Percentile")
+    
+    data_l = np.percentile(data, l_perc)
+    data_h = np.percentile(data, h_perc)
+    
+    #print (np.min(data), np.max(data), l_perc, h_perc, data_l, data_h)
+    
+    return data_l, data_h
+
+
+def load_loopread_csv(log_fn, test_name_dict):
     try:
-        data_df = pd.read_csv(fn)
+        data_df = pd.read_csv(log_fn, dtype={'timestamp': np.float64})
 
         bs = data_df['bs'].iloc[0]
         n_epochs = data_df["epoch"].iloc[-1]
@@ -36,16 +50,26 @@ def load_loopread_csv(fn):
         data_df["batch_bytes_MB"] = data_df["batch_bytes"] / 1e6  # batch size in MB
         data_df["data_rate_GBs"] = data_df["batch_bytes"] / data_df["batch_time"] / 1e9
         data_df["img_rate"] = bs / data_df["batch_time"]
-
+        data_df["datetime"] = pd.to_datetime(data_df.timestamp, unit='s')
+        
         data_grp_per_epoch = data_df.groupby("epoch")
     except:
-        print(f"cannot read csv file {fn}")
+        print(f"cannot read csv file {log_fn}")
         n_epochs = None
         bs = None
         data_df = None
         data_grp_per_epoch = None
 
-    return n_epochs, bs, data_df, data_grp_per_epoch
+    test_type = log_fn.split('AWS_MAIN_TEST_')[1].split('_')[0]
+    
+    basename = os.path.basename(log_fn)
+    
+    test_name, is_remote = test_name_dict[basename]
+    
+    if is_remote:
+        test_name = test_type + "lat - " + test_name
+    
+    return n_epochs, bs, data_df, data_grp_per_epoch, test_name
 
 
 def plot_data(
@@ -121,26 +145,32 @@ def get_per_epoch_avg_values(n_epochs, data_grp_per_epoch, data_field="batch_tim
             tmp_data_std = tmp_data.std()
         elif mean_type == 'harmonic':
             tmp_data_mean = hmean(tmp_data)
-            tmp_data_std = tmp_data.std()
+            tmp_data_std = get_conf_interval_hmean(tmp_data)
             
         avg_list.append(tmp_data_mean)
         std_list.append(tmp_data_std)
 
     return avg_list, std_list
 
+def moving_average(a, n=10):
+    a = a.values
+    ret = np.cumsum(a, dtype=float)
+    ret[n:] = ret[n:] - ret[:-n]
+    return ret[n - 1:] / n
+
 
 # -
 
 # #### Log parent folder
 
-parent_hi = "/home/giovanni/AWS_MAIN_TEST_HI_LAT/loopread/"
-parent_low = "/home/giovanni/AWS_MAIN_TEST_LOW_LAT/"
-parent_med = "/home/giovanni/AWS_MAIN_TEST_MED_LAT/"
+parent_hi = "./AWS_logs/AWS_MAIN_TEST_HI_LAT/loopread/"
+parent_low = "./AWS_logs/AWS_MAIN_TEST_LOW_LAT/"
+parent_med = "./AWS_logs/AWS_MAIN_TEST_MED_LAT/"
 
 # #### Read Dool as dataframe
 
 # +
-dool_fn = glob.glob(os.path.join(parent, "dool_loopread*"))[0]
+dool_fn = "./AWS_logs/AWS_MAIN_TEST_LOW_LAT/dool_loopread_2024-12-11.csv"
 dool_df = pd.read_csv(dool_fn, header=5, index_col=False)
 
 # Add unix epoch column
@@ -152,7 +182,9 @@ dool_df["timestamp"] = (
 dool_df.columns
 # -
 
-dool_df
+tmp = dool_df[dool_df.timestamp >= 1733881987.07862]
+tmp = tmp[tmp.timestamp < 1733882012.9338305]
+plt.plot(tmp['net/ens32:recv'])
 
 # #### Read loopread csv logs
 
@@ -170,18 +202,42 @@ csv_file_list = [glob.glob(i + "*.csv") for i in csv_folder_list]
 
 ## join list of lists
 csv_file_list = list(itertools.chain.from_iterable(csv_file_list))
-csv_file_list
+
+for ix, i in enumerate(csv_file_list):
+    print (ix, i)
 # -
 
+sel_csv_file_list = [csv_file_list[9], 
+                     csv_file_list[5], csv_file_list[17], csv_file_list[15],
+                     csv_file_list[1], csv_file_list[4], csv_file_list[3],
+                     csv_file_list[8], csv_file_list[18], csv_file_list[16]]
 
+test_name_dict = {
+    "AWS_loop_read_TF_tfdata_tfr_BS_512.csv" : ("TFdata_tfr", 0), 
+    "AWS_loop_read_TF_tfdataservice_tfr_BS_512.csv" : ("TFdataservice_tfr", 1),
+    "AWS_loop_read_TF_tfdata_files_BS_512.csv" : ("TFdata files", 0),
+    "AWS_loop_read_S3_Streaming_BS_512.csv" : ("S3_Streaming", 1),
+    "AWS_loop_read_pytorch_files_BS_512.csv" : ("Pytorch Files", 0),
+    "AWS_loop_read_cassandra_OOO_SLSTART_4_BS_512.csv" : ("Cassandra with OOO and SS=4", 1),
+    "AWS_loop_read_scylla_OOO_SLSTART_4_BS_512.csv" : ("Scylla with OOO and SS=4", 1),
+    "AWS_loop_read_scylla_OOO_SLSTART_0_BS_512.csv" : ("Scylla with OOO and SS=0", 1),
+    "AWS_loop_read_scylla_no_OOO_SLSTART_0_BS_512.csv" : ("Scylla without OOO and SS=0", 1),
+    "AWS_loop_read_DALI_tfrecord_BS_512.csv" : ("DALI tfr", 0),
+    "AWS_loop_read_DALI_file_BS_512.csv" : ("DALI files", 0),
+    "AWS_loop_read_S3_pytorch_files_BS_512.csv" : ("S3 Pytorch files", 1),
+    "AWS_loop_read_S3_DALI_file_BS_512.csv" : ("S3 DALI files", 1),
+}
 
 # ### Tests on single file
 
-fn = csv_file_list[11]
+fn = csv_file_list[12]
 
-n_epochs, bs, data_df, data_grp_per_epoch = load_loopread_csv(fn)
-print (n_epochs, bs)
+n_epochs, bs, data_df, data_grp_per_epoch, test_name = load_loopread_csv(fn, test_name_dict)
+print (test_name, n_epochs, bs)
 data_df
+
+print (f"Start: {data_df['datetime'].iloc[0]}")
+print (f"Stop: {data_df['datetime'].iloc[-1]}")
 
 # ## Batch Time
 
@@ -281,11 +337,18 @@ plot_data(
 
 # ### Tests on all files
 
+barcolor_dict = {'HIlat': 'r',
+             'MEDlat': 'g',
+             'LOWlat': 'b',
+             '': 'k'
+            }
+
 # +
 batch_time_dict = {}
 data_size_dict = {}
 data_rate_dict = {}
 img_rate_dict = {}
+df_dict = {}
 
 ep = 0
 x_bar = []
@@ -294,14 +357,25 @@ y_ds_bar = []
 y_dr_bar = []
 y_ir_bar = []
 x_tick_lab = []
+x_color=[]
 
-for i, fn in enumerate(csv_file_list):
-    print(fn)
-    n_epochs, bs, data_df, data_grp_per_epoch = load_loopread_csv(fn)
+for i, fn in enumerate(sel_csv_file_list):
+    #print(fn)
+    n_epochs, bs, data_df, data_grp_per_epoch, name = load_loopread_csv(fn, test_name_dict)
+    
+    test_type = name.split('-')[0].rstrip()
+    barcolor = "k"
+    
+    if test_type in list(barcolor_dict.keys()):
+        barcolor = barcolor_dict[test_type]
+        
+    print (test_type, list(barcolor_dict.keys()), barcolor)
+    
     if bs == None:
         continue
-    name = "_".join(os.path.basename(fn).split("_")[3:-2])
+        
     print(f"{name}")
+    
     avg_batch_time_list, std_batch_time_list = get_per_epoch_avg_values(
         n_epochs, data_grp_per_epoch, data_field="batch_time_ms"
     )
@@ -314,21 +388,24 @@ for i, fn in enumerate(csv_file_list):
     avg_img_rate_list, std_img_rate_list = get_per_epoch_avg_values(
         n_epochs, data_grp_per_epoch, data_field="img_rate", mean_type='harmonic'
     )
-
+    
     batch_time_dict[name] = (avg_batch_time_list, std_batch_time_list)
     data_size_dict[name] = (avg_data_size_list, std_data_size_list)
     data_rate_dict[name] = (avg_data_rate_list, std_data_rate_list)
     img_rate_dict[name] = (avg_img_rate_list, std_img_rate_list)
+    df_dict[name] = data_df
 
+    
     x_bar.append(i)
     y_bt_bar.append(avg_batch_time_list[ep])
     y_ds_bar.append(avg_data_size_list[ep])
     y_dr_bar.append(avg_data_rate_list[ep])
     y_ir_bar.append(avg_img_rate_list[ep])
     x_tick_lab.append(name)
+    x_color.append(barcolor)
 # -
 
-_ = plt.bar(x_bar, y_bt_bar)
+_ = plt.bar(x_bar, y_bt_bar / np.max(y_bt_bar), color=x_color)
 _ = plt.xticks(x_bar, x_tick_lab, rotation=90)
 plt.ylabel("average batch time (ms)")
 
@@ -336,16 +413,266 @@ _ = plt.bar(x_bar, y_ds_bar)
 _ = plt.xticks(x_bar, x_tick_lab, rotation=90)
 plt.ylabel("average batch size (MB)")
 
-_ = plt.bar(x_bar, y_dr_bar)
+_ = plt.bar(x_bar, y_dr_bar / np.max(y_dr_bar), color=x_color)
 _ = plt.xticks(x_bar, x_tick_lab, rotation=90)
 plt.ylabel("average batch data rate (GB/s)")
 
-_ = plt.bar(x_bar, y_ir_bar)
+_ = plt.bar(x_bar, y_ir_bar / np.max(y_ir_bar), color=x_color)
 _ = plt.xticks(x_bar, x_tick_lab, rotation=90)
 plt.ylabel("average batch image rate (img/s)")
 
+# # Paper figures
+
+barcolor_dict = {'HIlat': 'red',
+             'MEDlat': 'green',
+             'LOWlat': 'blue',
+             '': 'k'
+            }
+
+# +
+batch_time_dict = {}
+data_size_dict = {}
+data_rate_dict = {}
+img_rate_dict = {}
+df_dict = {}
+
+ep = 0 # Epoch to take
+
+x_bar = []
+y_bt_bar = []
+y_ds_bar = []
+y_dr_bar = []
+y_ir_bar = []
+y_bt_yerr = []
+y_ds_yerr = []
+y_dr_yerr = []
+y_ir_yerr = []
+x_tick_lab = []
+x_color=[]
+
+for i, fn in enumerate(sel_csv_file_list):
+    print('-'*100)
+    print(fn)
+    n_epochs, bs, data_df, data_grp_per_epoch, name = load_loopread_csv(fn, test_name_dict)
+    
+    test_type = name.split('-')[0].rstrip()
+    barcolor = "k"
+    
+    if test_type in list(barcolor_dict.keys()):
+        barcolor = barcolor_dict[test_type]
+
+    if bs == None:
+        continue
+    
+    avg_batch_time_list, std_batch_time_list = get_per_epoch_avg_values(
+        n_epochs, data_grp_per_epoch, data_field="batch_time_ms"
+    )
+    avg_data_size_list, std_data_size_list = get_per_epoch_avg_values(
+        n_epochs, data_grp_per_epoch, data_field="batch_bytes_MB"
+    )
+    avg_data_rate_list, std_data_rate_list = get_per_epoch_avg_values(
+        n_epochs, data_grp_per_epoch, data_field="data_rate_GBs", mean_type='harmonic'
+    )
+    avg_img_rate_list, std_img_rate_list = get_per_epoch_avg_values(
+        n_epochs, data_grp_per_epoch, data_field="img_rate", mean_type='harmonic'
+    )
+    
+    batch_time_dict[name] = (avg_batch_time_list, std_batch_time_list)
+    data_size_dict[name] = (avg_data_size_list, std_data_size_list)
+    data_rate_dict[name] = (avg_data_rate_list, std_data_rate_list)
+    img_rate_dict[name] = (avg_img_rate_list, std_img_rate_list)
+    df_dict[name] = data_df
+
+    
+    x_bar.append(i)
+    y_bt_bar.append(avg_batch_time_list[ep])
+    y_ds_bar.append(avg_data_size_list[ep])
+    y_dr_bar.append(avg_data_rate_list[ep])
+    y_ir_bar.append(avg_img_rate_list[ep])
+    
+    y_bt_yerr.append(std_batch_time_list[ep])
+    y_ds_yerr.append(std_data_size_list[ep])
+    y_dr_yerr.append(std_data_rate_list[ep])
+    y_ir_yerr.append(std_img_rate_list[ep])
+    
+    x_tick_lab.append(name)
+    x_color.append(barcolor)
+# -
+
+y_bar_np
 
 
 
+# ### Figure 3
+
+# +
+x_bar_np = np.array(x_bar)
+y_bar_np = np.array(y_dr_bar)
+x_color_np = np.array(x_color)
+x_tick_lab_np = ['DALI tfr', 'MosaicML SD', 'tf.data service', 'Cassandra-DALI']
+
+x_bar_loc_index = np.array([0])
+x_bar_hi_index = np.array([1, 4, 7])
+x_bar_med_index = np.array([2, 5, 8])
+x_bar_low_index = np.array([3, 6, 9])
+x_ticks_indexes = [0,2,5,8]
+x_offset = 0.2
+
+
+_ = plt.bar(x_bar_np[x_bar_loc_index], y_bar_np[x_bar_loc_index] / np.max(y_bar_np), color=x_color_np[x_bar_loc_index], label="Loc", zorder=3)
+_ = plt.bar(x_bar_np[x_bar_hi_index] + x_offset, y_bar_np[x_bar_hi_index] / np.max(y_bar_np), color=x_color_np[x_bar_hi_index], label="Hi", zorder=3)
+_ = plt.bar(x_bar_np[x_bar_med_index], y_bar_np[x_bar_med_index] / np.max(y_bar_np), color=x_color_np[x_bar_med_index], label="Med", zorder=3)
+_ = plt.bar(x_bar_np[x_bar_low_index] - x_offset, y_bar_np[x_bar_low_index] / np.max(y_bar_np), color=x_color_np[x_bar_low_index], label="Low", zorder=3)
+
+_ = plt.xticks(x_ticks_indexes, x_tick_lab_np, rotation=0)
+
+plt.ylabel("Normalized data rate")
+plt.legend(loc='upper center')
+plt.grid(axis='y', alpha=0.8, zorder=0)
+
+plt.savefig("figures/normalized_loopread.pdf", bbox_inches="tight")
+# -
+
+batch_time_dict
+
+# ### Figure 5
+
+# +
+scylla_ooo_fn = csv_file_list[8]
+scylla_no_ooo_fn = csv_file_list[12]
+epoch = 1
+
+n_epochs, bs, data_df, data_grp_per_epoch, test_name = load_loopread_csv(scylla_ooo_fn, test_name_dict)
+ooo_dr_series = data_grp_per_epoch.get_group(epoch)['batch_time_ms'].iloc[1:-1]
+ooo_dr_series_smoothed = moving_average(ooo_dr_series, n=1)
+print (ooo_dr_series.sum())
+m_ooo = np.mean(ooo_dr_series)
+
+n_epochs, bs, data_df, data_grp_per_epoch, test_name = load_loopread_csv(scylla_no_ooo_fn, test_name_dict)
+no_ooo_dr_series = data_grp_per_epoch.get_group(epoch)['batch_time_ms'].iloc[1:-1]
+no_ooo_dr_series_smoothed = moving_average(no_ooo_dr_series, n=1)
+print (no_ooo_dr_series.sum())
+m_no_ooo = np.mean(no_ooo_dr_series)
+
+f, axs = plt.subplots(2, 1, figsize=(10,7), sharex=True)
+axs[0].plot(ooo_dr_series_smoothed, label=f"OOO", c='k')
+axs[0].axhline(m_ooo, ls='--', c='r', label= f"mean={m_ooo:.2f} ms")
+axs[0].set_ylabel("milliseconds", fontsize=14)
+axs[0].grid(True)
+axs[0].legend(loc='upper center')
+
+axs[1].plot(no_ooo_dr_series_smoothed, label=f"no OOO", c='k', )
+axs[1].axhline(m_no_ooo, ls='--', c='r', label= f"mean={m_no_ooo:.2f} ms")
+axs[1].set_xlabel("batch index", fontsize=14)
+axs[1].set_ylabel("milliseconds", fontsize=14)
+axs[1].grid(True)
+axs[1].legend(loc='upper center')
+plt.tight_layout()
+
+plt.savefig("figures/batchtime_ooo_vs_noooo.pdf", bbox_inches="tight")
+# -
+
+plt.plot(no_ooo_dr_series_smoothed, label=f"no OOO", c='k', )
+plt.xlim(115,1000)
+for pippo in np.arange(119,1000,33):
+    plt.axvline(pippo, c='r')
+plt.grid()
+
+# +
+scylla_ooo_fn = csv_file_list[8]
+scylla_no_ooo_fn = csv_file_list[12]
+epoch = 2
+
+n_epochs, bs, data_df, data_grp_per_epoch, test_name = load_loopread_csv(scylla_ooo_fn, test_name_dict)
+ooo_dr_series = data_grp_per_epoch.get_group(epoch)['data_rate_GBs'].iloc[1:-1]
+ooo_dr_series_smoothed = moving_average(ooo_dr_series, n=1)
+m_ooo = hmean(ooo_dr_series)
+
+n_epochs, bs, data_df, data_grp_per_epoch, test_name = load_loopread_csv(scylla_no_ooo_fn, test_name_dict)
+no_ooo_dr_series = data_grp_per_epoch.get_group(epoch)['data_rate_GBs'].iloc[1:-1]
+no_ooo_dr_series_smoothed = moving_average(no_ooo_dr_series, n=1)
+m_no_ooo = hmean(no_ooo_dr_series)
+
+f, axs = plt.subplots(2, 1, figsize=(10,7), sharex=True)
+axs[0].plot(ooo_dr_series_smoothed, label="OOO", c='k')
+axs[0].axhline(m_ooo, ls='--', c='r', label= f"mean={m_ooo:.2f} GB/s")
+axs[0].set_ylabel("GB/s", fontsize=14)
+axs[0].grid(True)
+axs[0].legend(loc='upper center')
+
+axs[1].plot(no_ooo_dr_series_smoothed, label="no OOO", c='k')
+axs[1].axhline(m_no_ooo, ls='--', c='r', label= f"mean={m_no_ooo:.2f} GB/s")
+axs[1].set_xlabel("batch index", fontsize=14)
+axs[1].set_ylabel("GB/s", fontsize=14)
+axs[1].grid(True)
+axs[1].legend(loc='upper center')
+plt.tight_layout()
+
+plt.savefig("figures/throughput_ooo_vs_noooo.pdf", bbox_inches="tight")
+# -
+
+# ### Figure 8
+
+data_rate_dict['HIlat - Scylla with OOO and SS=4']
+
+
+
+fn = 'AWS_logs/server/stockholm/2024-12-10/general.csv'
+df_log = pd.read_csv(fn, header=5, index_col=False)
+df_log['time'] = '2024-' + df_log['time']
+df_log = df_log.set_index('time')
+# Convert index to datetime
+df_log.index = pd.to_datetime(df_log.index)
+#df_log.sort_index(inplace=True)
+df_log.columns
+
+len(df_log.columns)
+
+# +
+test_time_intervals_d = {'HI_scylla_ooo_start': '2024-12-10 16:03:31.660300032', 'HI_scylla_ooo_stop': '2024-12-10 16:05:58.601401856',
+                        'HI_cassandra_ooo_start': '2024-12-10 16:22:40.195471104', 'HI_cassandra_ooo_stop': '2024-12-10 16:28:41.880232960'}
+
+start_scylla = test_time_intervals_d['HI_scylla_ooo_start']
+stop_scylla = test_time_intervals_d['HI_scylla_ooo_stop']
+
+start_cass = test_time_intervals_d['HI_cassandra_ooo_start']
+stop_cass = test_time_intervals_d['HI_cassandra_ooo_stop']
+# -
+
+# #### Scylla
+
+# +
+##### Disk
+
+df_log_scylla = df_log.loc[start_scylla:stop_scylla].iloc[:]
+df_log_scylla['disk_IO:read'] = df_log_scylla[["dsk/nvm11:read", "dsk/nvm31:read", "dsk/nvm31:read", "dsk/nvm41:read"]].sum(axis=1) / 8e9
+
+scylla_disk_IO_mean = hmean(df_log_scylla['disk_IO:read'].values)
+scylla_disk_IO_interval = get_conf_interval_hmean(df_log_scylla['disk_IO:read'].values)
+
+print (scylla_disk_IO_mean, scylla_disk_IO_interval)
+
+#plt.plot(df_log_scylla[f"disk_IO:read"])
+
+##### Loopread Data rate 
+
+# -
+
+# #### Cass
+
+# +
+##### Disk
+df_log_cass = df_log.loc[start_cass:stop_cass]
+df_log_cass['disk_IO:read'] = df_log_cass[["dsk/nvm11:read", "dsk/nvm31:read", "dsk/nvm31:read", "dsk/nvm41:read"]].sum(axis=1) / 8e9
+
+cass_disk_IO_mean = hmean(df_log_cass['disk_IO:read'].values)
+cass_disk_IO_interval = get_conf_interval_hmean(df_log_cass['disk_IO:read'].values)
+
+print (cass_disk_IO_mean, cass_disk_IO_interval)
+
+#plt.plot(df_log_cass[f"disk_IO:read"])
+
+##### Loopread Data rate
+# -
 
 
